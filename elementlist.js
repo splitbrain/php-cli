f8c6c638a09c9da532a16e55e4f5ff3276eae380
Travis <travis@travis-ci.org>
2019-12-12 08:26:25 +0000

    Docs updated by Travis

---
+++ elementlist.js
@@ -0,0 +1,3 @@
+
+var ApiGen = ApiGen || {};
+ApiGen.elements = [["c","splitbrain\\phpcli\\CLI"],["c","splitbrain\\phpcli\\Colors"],["c","splitbrain\\phpcli\\Exception"],["c","splitbrain\\phpcli\\Options"],["c","splitbrain\\phpcli\\PSR3CLI"],["c","splitbrain\\phpcli\\TableFormatter"]];
